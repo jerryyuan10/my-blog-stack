dcbdf77bc9e7c4cd375748fa3f5b040b2dcbfad7
jerryyuan10 <71941049+jerryyuan10@users.noreply.github.com>
2021-06-21 12:03:25 -0500

    Created Front End & Back End
    my-blog is front end.
    my-blog-backend is back end.

---
+++ my-blog/src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>
+        Welcome to my blog! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <p>
+        Tempor commodo ullamcorper a lacus vestibulum sed. Blandit volutpat maecenas volutpat blandit. Euismod lacinia at quis risus sed vulputate odio. Sapien faucibus et molestie ac. Vitae purus faucibus ornare suspendisse sed nisi. Amet nisl purus in mollis. Fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Quis commodo odio aenean sed adipiscing diam donec adipiscing. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Odio morbi quis commodo odio aenean sed adipiscing. Viverra mauris in aliquam sem fringilla ut morbi tincidunt augue. Mattis aliquam faucibus purus in massa. Elit ut aliquam purus sit. Sem integer vitae justo eget. Cras ornare arcu dui vivamus arcu felis bibendum.
+    </p>
+    <p>
+        Suscipit adipiscing bibendum est ultricies integer quis. Bibendum enim facilisis gravida neque convallis a cras semper auctor. Vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt. Feugiat in fermentum posuere urna. Id velit ut tortor pretium viverra suspendisse potenti. Vestibulum sed arcu non odio euismod lacinia at quis. Ac turpis egestas integer eget aliquet. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl. Nulla malesuada pellentesque elit eget gravida cum sociis. Mattis ullamcorper velit sed ullamcorper. Nunc mattis enim ut tellus elementum sagittis.
+    </p>
+    </>
+);
+
+export default HomePage;
